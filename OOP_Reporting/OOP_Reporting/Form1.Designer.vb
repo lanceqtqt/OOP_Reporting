90c49ebf994f1e7c7f60c583668f22814be1e6b6
Ransuru <165863612+lanceqtqt@users.noreply.github.com>
2024-11-02 11:36:39 +0800

    Add files via upload

---
+++ OOP_Reporting/OOP_Reporting/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        add_btn = New Button()
+        DataGridView1 = New DataGridView()
+        edit_btn = New Button()
+        delete_btn = New Button()
+        Button1 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' add_btn
+        ' 
+        add_btn.Location = New Point(47, 35)
+        add_btn.Name = "add_btn"
+        add_btn.Size = New Size(93, 30)
+        add_btn.TabIndex = 0
+        add_btn.Text = "Add Row"
+        add_btn.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(188, 23)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(343, 245)
+        DataGridView1.TabIndex = 1
+        ' 
+        ' edit_btn
+        ' 
+        edit_btn.Location = New Point(47, 93)
+        edit_btn.Name = "edit_btn"
+        edit_btn.Size = New Size(93, 30)
+        edit_btn.TabIndex = 2
+        edit_btn.Text = "Edit Row"
+        edit_btn.UseVisualStyleBackColor = True
+        ' 
+        ' delete_btn
+        ' 
+        delete_btn.Location = New Point(47, 150)
+        delete_btn.Name = "delete_btn"
+        delete_btn.Size = New Size(93, 30)
+        delete_btn.TabIndex = 3
+        delete_btn.Text = "Delete Row"
+        delete_btn.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(47, 207)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(93, 30)
+        Button1.TabIndex = 4
+        Button1.Text = "Cancel Delete"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(588, 333)
+        Controls.Add(Button1)
+        Controls.Add(delete_btn)
+        Controls.Add(edit_btn)
+        Controls.Add(DataGridView1)
+        Controls.Add(add_btn)
+        MinimumSize = New Size(604, 372)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents add_btn As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents edit_btn As Button
+    Friend WithEvents delete_btn As Button
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ OOP_Reporting/OOP_Reporting/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Data
+
+Public Class Form1
+    ' Define a DataSet to hold your tables
+    Dim dataBooks As New DataSet()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Create a New DataTable called "Titles"
+        Dim titlesTable As New DataTable("Titles")
+
+        ' Add columns to the "Titles" DataTable
+        titlesTable.Columns.Add("id", GetType(Integer))
+        titlesTable.Columns.Add("title", GetType(String))
+        titlesTable.Columns.Add("type", GetType(String))
+
+        ' Add the "Titles" table to the DataSet
+        dataBooks.Tables.Add(titlesTable)
+
+        ' Bind the DataGridView to the Titles table
+        DataGridView1.DataSource = dataBooks.Tables("Titles")
+    End Sub
+
+    Private Sub add_btn_Click(sender As Object, e As EventArgs) Handles add_btn.Click
+        ' Create a new DataRow for the "Titles" table
+        Dim newDataRow = dataBooks.Tables("Titles").NewRow()
+
+        ' Set column values
+        newDataRow("id") = dataBooks.Tables("Titles").Rows.Count + 1 ' Automatically increment ID
+        newDataRow("title") = "New York"
+        newDataRow("type") = "business"
+
+        ' Add the new row to the "Titles" table
+        dataBooks.Tables("Titles").Rows.Add(newDataRow)
+
+        ' Optional: Refresh the DataGridView to display the new data immediately
+        DataGridView1.Refresh()
+    End Sub
+
+    Private Sub edit_btn_Click(sender As Object, e As EventArgs) Handles edit_btn.Click
+        ' Check if there are rows to edit
+        If DataGridView1.SelectedRows.Count > 0 Then
+            ' Get the index of the selected row
+            Dim selectedIndex As Integer = DataGridView1.SelectedRows(0).Index
+
+            ' Check if the selected index is within the bounds of the DataTable
+            If selectedIndex >= 0 AndAlso selectedIndex < dataBooks.Tables("Titles").Rows.Count Then
+                ' Get the selected row of the "Titles" table
+                Dim editDataRow As DataRow = dataBooks.Tables("Titles").Rows(selectedIndex)
+
+                ' Begin editing the row
+                editDataRow.BeginEdit()
+
+                ' Update the "Title" column with the new value
+                editDataRow("type") = 23
+
+                ' End the edit
+                editDataRow.EndEdit()
+
+                ' Refresh the DataGridView to reflect the changes
+                DataGridView1.Refresh()
+            Else
+                MessageBox.Show("No rows available to edit.")
+            End If
+        Else
+            MessageBox.Show("Please select a row to edit.")
+        End If
+    End Sub
+
+    Private Sub delete_btn_Click(sender As Object, e As EventArgs) Handles delete_btn.Click
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim selectedIndex = DataGridView1.SelectedRows(0).Index
+            If selectedIndex >= 0 AndAlso selectedIndex < dataBooks.Tables("Titles").Rows.Count Then
+                Dim delRow = dataBooks.Tables("Titles").Rows(selectedIndex)
+
+                ' Mark the row for deletion
+                delRow.Delete()
+
+                ' Refresh the DataGridView to reflect the deletion
+                DataGridView1.Refresh()
+            Else
+                MessageBox.Show("No rows available to delete.")
+            End If
+        Else
+            MessageBox.Show("Please select a row to delete.")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Discard changes made to the DataTable
+        If dataBooks.Tables("Titles").GetChanges() IsNot Nothing Then
+            MessageBox.Show("Changes have been canceled.")
+
+            dataBooks.Tables("Titles").RejectChanges() ' Revert all changes made
+            DataGridView1.Refresh() ' Refresh the DataGridView to show the original data
+        Else
+            MessageBox.Show("There are no changes to cancel.")
+        End If
+    End Sub
+End Class
